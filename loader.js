4a9800f3bc9600703ad052ce556b652938b54224
Abeer Khan <140332219+abeerlala@users.noreply.github.com>
2025-08-09 14:14:28 +0500

    Added All Files Except Images Folder

---
+++ loader.js
@@ -0,0 +1,134 @@
+// Load Navigation Bar On All Pages
+
+fetch("/Components/nav.html")
+    .then(res => res.text())
+    .then(data => { document.getElementById("nav").innerHTML = data; })
+    .then(() => {
+        const checkbox = document.querySelector("#menu");
+        const navList = document.querySelector(".navList");
+        const label = document.querySelector(".label");
+        checkbox.addEventListener("change", () => {
+            if (checkbox.checked) {
+                // navList.style.display = "flex";
+                navList.style.left = "0";
+                label.classList.remove("fa-bars")
+                label.classList.add("fa-xmark")
+            } else {
+                // navList.style.display = "none";
+                navList.style.left = "-100%";
+                label.classList.remove("fa-xmark")
+                label.classList.add("fa-bars")
+            }
+        })
+    })
+    .then(() => {
+        const pageName = location.href.split("/").pop();
+        const navLinks = document.querySelectorAll(".navLink");
+        const homeLink = document.querySelector(".navLink");
+        navLinks.forEach((link) => {
+            if (link.getAttribute("href") == pageName) {
+                link.classList.add("activeNavLink")
+            } else if (
+                pageName.includes("services") ||
+                pageName.includes("techSupport") ||
+                pageName.includes("training") ||
+                pageName.includes("parts")
+            ) {
+                document.querySelector(".serviceDrop").classList.add("activeA");
+                document.querySelector(".dropbtnSpecial2").classList.add("activeATwo");
+                document.querySelectorAll(".thirdLink").forEach(link => {
+                    if (pageName.includes(link.getAttribute("href").split("/").pop())) {
+                        link.style.fontWeight = "800";
+                        link.style.color = "var(--4)";
+                        link.style.backgroundColor = "var(--1)";
+                    }
+                });
+                document.querySelectorAll(".secondLinkSpecial").forEach(link => {
+                    if (pageName.includes(link.getAttribute("href"))) {
+                        link.style.fontWeight = "800";
+                        link.style.color = "var(--4)";
+                        link.style.backgroundColor = "var(--1)";
+                    }
+                });
+
+            } else if (pageName == "") {
+                homeLink.classList.add("activeNavLink")
+            } else if (
+                pageName.includes("products") ||
+                pageName.includes("uv") ||
+                pageName.includes("dtf") ||
+                pageName.includes("ecosolvent") ||
+                pageName.includes("sublimation") ||
+                pageName.includes("laser") ||
+                pageName.includes("plotter") ||
+                pageName.includes("heatTransfer") ||
+                pageName.includes("inks") ||
+                pageName.includes("material")
+            ) {
+                document.querySelector(".productDrop").classList.add("activeA");
+                document.querySelector(".dropbtnSpecial").classList.add("activeATwo");
+                document.querySelectorAll(".secondLink").forEach(link => {
+                    if (pageName.includes(link.getAttribute("href").split("/").pop())) {
+                        link.style.fontWeight = "800";
+                        link.style.color = "var(--4)";
+                        link.style.backgroundColor = "var(--1)";
+                    }
+                });
+            }
+        })
+        const input = document.getElementById("search");
+        const ul = document.getElementById("myUL");
+
+        // Show dropdown on focus
+        input.addEventListener("focus", () => {
+            ul.style.display = "flex";
+        });
+
+        // Hide dropdown on click outside
+        document.addEventListener("click", (e) => {
+            if (!input.contains(e.target) && !ul.contains(e.target)) {
+                ul.style.display = "none";
+            }
+        });
+
+        const searchShowBtn = document.querySelector(".activeSearchBar");
+        const searchBar = document.querySelector(".search2");
+        let searchBarShowed = false;
+        searchShowBtn.addEventListener("click", () => {
+            if (searchBarShowed === false) {
+                searchBar.style.top = "70px";
+                searchBarShowed = true;
+            } else if (searchBarShowed) {
+                searchBar.style.top = "0";
+                searchBarShowed = false;
+            }
+        });
+
+        const ul1 = document.querySelector("#myUL");
+        ul1.querySelectorAll(".searchBtn").forEach(btn => {
+            btn.href = `${btn.href}#${btn.innerText.replaceAll(' ', '')}`;
+        })
+
+    });
+
+// Load Search Bar For Mobile 
+
+fetch("/Components/search2.html")
+    .then(res => res.text())
+    .then(data => { document.getElementById("searchcontainer").innerHTML = data; })
+    .then(() => {
+        const input2 = document.getElementById("search2");
+        const ul2 = document.getElementById("myUL2");
+        input2.addEventListener("focus", () => {
+            ul2.style.display = "flex";
+        });
+        document.addEventListener("click", (e) => {
+            if (!input2.contains(e.target) && !ul2.contains(e.target)) {
+                ul2.style.display = "none";
+            }
+        });
+
+        ul2.querySelectorAll(".searchBtn").forEach(btn => {
+            btn.href = `${btn.href}#${btn.innerText.replaceAll(' ', '')}`;
+        })
+    })
---
+++ script.js
@@ -0,0 +1,206 @@
+AOS.init();
+
+// ToTop Logic
+
+const toTop = document.querySelector("#toTop");
+document.addEventListener("scroll", () => {
+    scrollY > 300 ? toTop.style.display = "grid" : toTop.style.display = "none";
+});
+
+toTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// Home Page Slide's Logic
+
+const pageName = location.href.split("/").pop();
+if (pageName == "" || pageName == "index.html") {
+    let swiper;
+
+    document.addEventListener('DOMContentLoaded', function () {
+        swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.carouselRight',
+                prevEl: '.carouselLeft',
+            },
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            speed: 1500
+        });
+
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "ArrowRight") {
+                swiper.slideNext(); // Move to next slide
+            } else if (e.key === "ArrowLeft") {
+                swiper.slidePrev(); // Move to previous slide
+            }
+        });
+
+        const pausePlayIcon = document.getElementById('pausePlayIcon');
+        let isPaused = false;
+
+        pausePlayIcon.addEventListener('click', () => {
+            if (isPaused) {
+                swiper.autoplay.start();
+                pausePlayIcon.classList.remove('fa-play');
+                pausePlayIcon.classList.add('fa-pause');
+            } else {
+                swiper.autoplay.stop();
+                pausePlayIcon.classList.remove('fa-pause');
+                pausePlayIcon.classList.add('fa-play');
+            }
+            isPaused = !isPaused;
+        });
+    });
+}
+
+// Navbar Dropdown Logic
+
+function myFunction() {
+    document.getElementById("myDropdown").style.display = "flex";;
+}
+function myFunction2() {
+    document.getElementById("myDropdown").style.display = "none";;
+}
+
+function active() {
+    document.getElementById("myDropdown2").style.display = "flex";;
+}
+function active2() {
+    document.getElementById("myDropdown2").style.display = "none";;
+}
+
+// Product Cards Logic
+
+const images = document.querySelectorAll(".imageProduct img");
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        const src = img.src;
+        window.open(src)
+    });
+});
+
+const productCard = document.querySelectorAll(".productCard");
+productCard.forEach(card => {
+    const img = card.querySelector("img");
+    const a = card.querySelector("a");
+    const title = card.querySelector("div.product-title");
+    a.href = `${a.href}Hello, I’m interested in the ${img.alt}. Kindly send me the price and availability of the ${img.alt}`;
+    card.id = title.innerText.replaceAll(' ', '');
+});
+
+// Material Page Logic
+
+const tabBtns = document.querySelectorAll(".tabBtn");
+tabBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const btnTarget = btn.classList.item(1);
+        document.querySelector(`#${btnTarget}`).scrollIntoView({ block: "center" })
+    });
+});
+
+if (pageName == "material.html") {
+    const tabs = document.querySelector(".tabs");
+    const tabToggleBtn = tabs.querySelector(".fa-solid");
+    let showed = true;
+    tabToggleBtn.addEventListener("click", () => {
+        if (showed) {
+            tabToggleBtn.classList.remove("fa-angle-up")
+            tabToggleBtn.classList.add("fa-angle-down")
+            tabBtns.forEach(btn => { btn.style.display = "none"; });
+            showed = false;
+            tabToggleBtn.parentElement.style.width = "fit-content";
+        } else if (showed == false) {
+            tabToggleBtn.classList.add("fa-angle-up")
+            tabToggleBtn.classList.remove("fa-angle-down")
+            tabBtns.forEach(btn => { btn.style.display = "block"; });
+            showed = true;
+            tabToggleBtn.parentElement.style.width = "100%";
+        }
+    });
+}
+
+// Search Functions
+
+function filterSearch() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search2");
+    filter = input.value.trim().toUpperCase();
+    ul = document.querySelector("#myUL2");
+    li = ul.querySelectorAll("li");
+    let anyVisible = false;
+    for (i = 0; i < li.length; i++) {
+        a = li[i].querySelector("a");
+        if (a) {
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+                anyVisible = true;
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+    let noResults = document.getElementById("noResults2");
+    if (!anyVisible) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
+    }
+}
+
+function filterSearch2() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.trim().toUpperCase();
+    ul = document.querySelector("#myUL");
+    li = ul.querySelectorAll("li");
+    let anyVisible = false;
+    for (i = 0; i < li.length; i++) {
+        a = li[i].querySelector("a");
+        if (a) {
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+                anyVisible = true;
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+    let noResults = document.getElementById("noResults");
+    if (!anyVisible) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (screen.width <= 600) {
+        document.querySelectorAll(".productCard").forEach(el => el.classList.remove("specialCard"))
+        document.querySelectorAll(".productCardInner1").forEach(el => el.classList.remove("productCardInner1"))
+        document.querySelectorAll(".imageProduct").forEach(el => el.classList.remove("specialImage"))
+        document.querySelectorAll(".product-details").forEach(el => el.classList.remove("product-detailsspecial"))
+        document.querySelectorAll(".getAQuote").forEach(el => el.classList.remove("specialQuote"))
+        document.querySelectorAll(".imageProduct img").forEach(el => { el.style.height = "" })
+
+        if (location.href.split("/").pop().includes("ecosolvent.html") || location.href.split("/").pop().includes("ecosolvent")) {
+            const image1 = document.querySelector("#specialImage1");
+            const image2 = document.querySelector("#specialImage2");
+            image1.src = "Images/Products/es2.webp";
+            image2.src = "Images/Products/es1.webp";
+            image1.parentElement.style.width = "40%";
+            image2.parentElement.style.width = "60%";
+        }
+    }
+});
